6b20abfd50a637c81877fc6bb00125b86b6c28df
Ricky <ttpss861220@gmail.com>
2020-04-14 22:05:08 +0800

    123

---
+++ 課堂練習/CityDropdownlist/MyHelper.js
@@ -0,0 +1,13 @@
+function $g(selector){
+    if(selector.includes('#') && !selector.includes(' ')){
+        return document.querySelector(selector);
+    }
+
+    let nodelist = document.querySelectorAll(selector);
+    return nodelist.length == 1 ? nodelist[0] : nodelist;
+}
+
+
+function $c(selector){
+    return document.createElement(selector);
+}
